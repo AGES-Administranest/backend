0340cec29f697bf01d06065e9d5a856b9a6b3f7b
MatheusMasera12 <matheusmasera11@gmail.com>
2026-09-09 18:52:26 -0300

    chore(stock): scaffold stock module and register in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { PrismaModule } from './infra/prisma/prisma.module';
 import { AuthModule } from './modules/auth';
 import { UsersModule } from './modules/users/users.module';
+import { StockModule } from './modules/stock/stock.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UsersModule } from './modules/users/users.module';
     PrismaModule,
     UsersModule,
     AuthModule,
+    StockModule
   ],
   controllers: [AppController],
   providers: [AppService],
